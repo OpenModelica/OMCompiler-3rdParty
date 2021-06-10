9f59ad57a81068fd96a0d79d200592a6d7e378a4
Martin Sjölund <martin@sjoelund.se>
2021-06-10 12:44:01 +0200

    Make f2c integer always be int

--- Cdaskr/solver/ddaskr_types.h
+++ Cdaskr/solver/ddaskr_types.h
@@ -6,14 +6,8 @@ typedef enum { _FALSE_, _TRUE_ } _boolean;
 
 typedef double real_number;
 
-#if defined(__alpha__) || defined(__sparc64__) || defined(__x86_64__) || defined(__ia64__)
 typedef int integer;
 typedef unsigned int uinteger;
-#else
-typedef long int integer;
-typedef unsigned long int uinteger;
-#endif
-
 #endif
 
 #ifdef __cplusplus
