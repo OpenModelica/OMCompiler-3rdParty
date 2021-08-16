0033c56b5d975fcb6dc1b3d7d32a30ffab2a7658
Mahder Gebremedhin <mahder.gebremedhin@liu.se>
2021-08-16 15:31:32 +0200

    [cmake] Add CMake configuration support for Lis
      - Only tested on Linux for now. We will check Windows later.

---
+++ lis-1.4.12/include/lis_config.h.in.cmake
@@ -0,0 +1,139 @@
+/* include/lis_config.h.  Generated from lis_config.h.in.cmake by CMake.  */
+
+/* Define to dummy `main' function (if any) required to link to the Fortran
+   libraries. */
+#cmakedefine F77_DUMMY_MAIN
+
+/* Define to a macro mangling the given C identifier (in lower and upper
+   case), which must not contain underscores, for linking with Fortran. */
+#cmakedefine F77_FUNC
+
+/* As F77_FUNC, but for C identifiers containing underscores. */
+#cmakedefine F77_FUNC_
+
+/* Define if F77 and FC dummy `main' functions are identical. */
+#cmakedefine FC_DUMMY_MAIN_EQ_F77
+
+/* Define to 1 to enable quad precision operations using FMA. */
+#cmakedefine HAS_FMA
+
+/* Define to 1 to enable x87 FPU. */
+#cmakedefine HAS_X87_FPU 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <malloc.h> header file. */
+#cmakedefine HAVE_MALLOC_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#cmakedefine HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#cmakedefine LT_OBJDIR
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT @PACKAGE_BUGREPORT@
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME @PACKAGE_NAME@
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING @PACKAGE_STRING@
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME @PACKAGE_TARNAME@
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL @PACKAGE_URL@
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION @PACKAGE_VERSION@
+
+/* Test the size of a `double', as computed by sizeof. */
+#cmakedefine SIZEOF_DOUBLE @SIZEOF_DOUBLE@
+
+/* Test the size of a `float', as computed by sizeof. */
+#cmakedefine SIZEOF_FLOAT @SIZEOF_FLOAT@
+
+/* Test the size of a `int', as computed by sizeof. */
+#cmakedefine SIZEOF_INT @SIZEOF_INT@
+
+/* Test the size of a `long', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG @SIZEOF_LONG@
+
+/* Test the size of a `long double', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG_DOUBLE @SIZEOF_LONG_DOUBLE@
+
+/* Test the size of a `long long', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG_LONG @SIZEOF_LONG_LONG@
+
+/* Test the size of a `size_t', as computed by sizeof. */
+#cmakedefine SIZEOF_SIZE_T @SIZEOF_SIZE_T@
+
+/* Test the size of a `void *', as computed by sizeof. */
+#cmakedefine SIZEOF_VOID_P @SIZEOF_VOID_P@
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS 1
+
+/* Define to 1 to build quad precision operations using fast quad add. */
+#cmakedefine USE_FAST_QUAD_ADD
+
+/* Define to 1 to enable quad precision operations using FMA and SSE2. */
+#cmakedefine USE_FMA2_SSE2
+
+/* Define to 1 to enable Fortran 77 subroutines. */
+#cmakedefine USE_FORTRAN
+
+/* Use MAIN__ macro with Fortran. */
+#cmakedefine USE_MAIN__
+
+/* Define to 1 to enable overlapping of lis_sendrecv. */
+#cmakedefine USE_OVERLAP
+
+/* Define to 1 to enable quad precision operations. */
+#cmakedefine USE_QUAD_PRECISION
+
+/* Define to 1 to enable SA-AMG preconditioner. */
+#cmakedefine USE_SAAMG
+
+/* Define to 1 to enable quad precision operations using SSE2. */
+#cmakedefine USE_SSE2
+
+/* Define to 1 to use vectorization. */
+#cmakedefine USE_VEC_COMP
+
+/* Define to empty if `const' does not conform to ANSI C. */
+#cmakedefine const
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+#cmakedefine size_t
