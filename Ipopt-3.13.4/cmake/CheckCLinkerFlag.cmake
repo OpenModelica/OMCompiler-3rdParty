594fd3171400dd04e8ea0dc7b35277e344b4df60
Mahder Gebremedhin <mahder.geb@gmail.com>
2022-02-21 11:38:12 +0100

    Minor cleanup for CMake build.
      - Set status messages as `STATUS`
      - Fix installation lib dirs from just `lib` to `${CMAKE_INSTALL_LIBDIR}`
      - Change `pkgconfig` to `pkgconfig`

--- Ipopt-3.13.4/cmake/CheckCLinkerFlag.cmake
+++ Ipopt-3.13.4/cmake/CheckCLinkerFlag.cmake
@@ -3,7 +3,7 @@
 #
 #  FLAG - the compiler flag
 #  VARIABLE - variable to store the result
-# 
+#
 #  This actually calls the check_c_source_compiles macro.
 #  See help for CheckCSourceCompiles for a listing of variables
 #  that can modify the build.
@@ -16,7 +16,7 @@ include(CheckCSourceRuns)
 
 macro(CHECK_C_LINKER_FLAG _FLAG _RESULT)
    set(CMAKE_REQUIRED_FLAGS "${_FLAG}")
-   message(status "check linker flag - test linker flags: ${CMAKE_REQUIRED_FLAGS}")
+   message(STATUS "check linker flag - test linker flags: ${CMAKE_REQUIRED_FLAGS}")
    check_c_source_runs("int main() { return 0;}" ${_RESULT})
    set(CMAKE_REQUIRED_FLAGS "")
 endmacro(CHECK_C_LINKER_FLAG)
