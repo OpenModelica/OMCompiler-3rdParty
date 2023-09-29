379cbc14345834820cfe28e2787035072661ac66
Adrian Pop <adrian.pop@liu.se>
2023-09-29 10:33:10 +0200

    add OM formatting to RYU library (#148)

---
+++ ryu/ryu/om_format.c
@@ -0,0 +1,272 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Open Source Modelica Consortium (OSMC),
+ * c/o Linköpings universitet, Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF THE BSD NEW LICENSE OR THE
+ * GPL VERSION 3 LICENSE OR THE OSMC PUBLIC LICENSE (OSMC-PL) VERSION 1.2.
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+ * RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GPL VERSION 3,
+ * ACCORDING TO RECIPIENTS CHOICE.
+ *
+ * The OpenModelica software and the OSMC (Open Source Modelica Consortium)
+ * Public License (OSMC-PL) are obtained from OSMC, either from the above
+ * address, from the URLs: http://www.openmodelica.org or
+ * http://www.ida.liu.se/projects/OpenModelica, and in the OpenModelica
+ * distribution. GNU version 3 is obtained from:
+ * http://www.gnu.org/copyleft/gpl.html. The New BSD License is obtained from:
+ * http://www.opensource.org/licenses/BSD-3-Clause.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, EXCEPT AS
+ * EXPRESSLY SET FORTH IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE
+ * CONDITIONS OF OSMC-PL.
+ *
+ */
+
+#include "ryu.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+
+#define min(x,y) ((x) < (y) ? (x) : (y))
+
+/* code by casella (https://github.com/casella), see:
+ * https://github.com/OpenModelica/OpenModelica/issues/7465
+ * Takes the string output of Ryu's d2s() function as input and writes
+ * the minimal decimal or exponential version of it to a buffer
+ * str1: the string output from d2s()
+ * buf: a character array (32 chars should be more than enough in all cases)
+ * real_output: if true, decimal output of round numbers
+ *              will have trailing .0, as in 1.0 or 1240.0
+ *              if false it will be displayed as an integer 1 or 1240
+ */
+void ryu_to_hr(const char *str1, char *buf, int real_output) 
+{
+  int sign;  // Sign of the number
+  char digits[32] = {0};  // Digits of the mantissa
+  int ndec; // Number of digits after decimal point in the input
+  int exp;  // Exponent in the input
+  char str2[32] = {0}; // Stores the decimal output
+  const char *ptr1 = str1; // Movable pointer to the input
+  char *ptr2 = str2; // Movable pointer to the decimal output
+  char *ptr = digits; // Movable pointer to the digits string
+  int skip_dec = 0; // If true, skip the decimal conversion
+  int i;
+
+  // Parse sign, digits, ndec, exp from input
+
+  if(!strchr(str1, 'E') && !strchr(str1, 'e')) {
+    // The input string is not in (-)xx.xEyy format
+    skip_dec = 1;
+  } else {
+    // Compute sign
+    if(*ptr1 == '-') {
+      sign = -1;
+      ptr1++;
+    } else {
+      sign = +1;
+    }
+    // Compute digits string
+    while(*ptr1 != 'E' && *ptr1 != 'e')
+      *ptr++ = *ptr1++;  // Copy the digit part of the input to the digits string
+    // Compute ndec
+    ndec = strchr(digits, '.') ? strlen(digits) - 2 : 0;
+    // Compute exp
+    sscanf(++ptr1, "%d", &exp);
+  }
+  if(exp > 5 || exp < -3) {
+    // Decimal format too long, skip it (and avoid buffer overflow in str2
+    skip_dec = 1;
+  } else if (!skip_dec) {
+    // Fill str2 with the digital form
+    // Print the sign to str2
+    if(sign < 0)
+      *ptr2++ = '-';
+    // Print the rest of the number to str2
+    ptr = digits; // Rewind pointer to digits
+    if(exp == 0) {
+      strcpy(ptr2, digits);  // Trivial, the output is just digits
+    } else if(exp > 0) { // Here you need to move the decimal point to the right
+      *ptr2++ = *ptr++; // Copy first digit to str2
+      ptr++; // Skip the decimal point
+      for(i = 0; i < min(ndec, exp); i++)
+        *ptr2++ = *ptr++; // Copy the digits up to the new decimal point
+      if(exp > ndec) { // Trailing zeros are needed
+        for(i = 0; i < exp - ndec; i++)
+          *ptr2++ = '0'; // Add trailing zeros before decimal point
+      } else if(exp < ndec) { // Remaining digits after decimal point
+        *ptr2++ = '.'; // Add decimal point
+        strcpy(ptr2, ptr); // Add remaining digits after decimal point
+      }
+    } else if(exp < 0) { // Number starts with "0.", then some zeros
+      // Add "0." to str2
+      *ptr2++ = '0';
+      *ptr2++ = '.';
+      for(i = 0; i < (-exp - 1); i++)
+        *ptr2++ = '0'; // Add leading zeros after decimal point in str2
+      *ptr2++ = *ptr++; // Add first digit to str2
+      if(ndec > 0)
+        ptr++; // Skip decimal point in digits
+      strcpy(ptr2, ptr); // Add remaining digits to str2
+    }
+    if (exp >= ndec && real_output)  // Round decimal output, real output required
+      strcat(str2, ".0");
+  }
+  if(exp < -3 || exp > 5 || (exp > 0 && exp - ndec > 3) || skip_dec)
+  {
+    // use the exponential form (change 'e' to 'E')
+    int i = 0;
+    while (str1[i])
+      buf[i] = str1[i] == 'E' ? 'e' : str1[i];
+    buf[i] = '\0';
+  }
+  else
+    strcpy(buf, str2); // use the decimal form
+}
+
+
+#if defined(TEST_RYU_TO_HR)
+void test(const char *d2s, const char *expected)
+{
+  char buf[32];
+  printf("%s -> ", d2s);
+  ryu_to_hr(d2s, buf, 0);
+  printf("%s", buf);
+  printf("/%s\n", expected);
+}
+
+void test_real (const char *d2s, const char *expected)
+{
+  char buf[32];
+  printf("%s -> ", d2s);
+  ryu_to_hr(d2s, buf, 1);
+  printf("%s", buf);
+  printf("/%s\n", expected);
+}
+
+int main() 
+{
+  // Try all possible combinations of exp and ndec to ensure complete coverage
+  printf("d2s -> output / expected\n");
+  test("8e5", "8e5");
+  test("8e4", "8e4");
+  test("8e3", "8000");
+  test("8e2", "800");
+  test("8e1", "80");
+  test("8e0", "8");
+  test("8e-1", "0.8");
+  test("8e-2", "0.08");
+  test("8e-3", "0.008");
+  test("8e-4", "8e-4");
+  test("8e-5", "8e-5");
+  test("8.13e6", "8.13e6");
+  test("8.13e5", "813000");
+  test("8.13e4", "81300");
+  test("8.13e3", "8130");
+  test("8.13e2", "813");
+  test("8.13e1", "81.3");
+  test("8.13e0", "8.13");
+  test("8.13e-1", "0.813");
+  test("8.13e-2", "0.0813");
+  test("8.13e-3", "0.00813");
+  test("8.13e-4", "8.13e-4");
+  test("8.13e-5", "8.13e-5");
+  test("8.1234567e6", "8.1234567e6");
+  test("8.1234567e5", "812345.67");
+  test("8.1234567e4", "81234.567");
+  test("8.1234567e3", "8123.4567");
+  test("8.1234567e2", "812.34567");
+  test("8.1234567e1", "81.234567");
+  test("8.1234567e0", "8.1234567");
+  test("8.1234567e-1", "0.81234567");
+  test("8.1234567e-2", "0.081234567");
+  test("8.1234567e-3", "0.0081234567");
+  test("8.1234567e-4", "8.1234567e-4");
+  test("8.1234567e-5", "8.1234567e-5");
+  test("-1.2e1", "-12");
+  test("-4.56e8", "-4.56e8");
+  test("1e-60", "1e-60");
+  test("1e80", "1e80");
+  test("NaN", "NaN");
+  test("Inf", "Inf");
+  test("-Inf", "-Inf");
+
+  test_real("8e5", "8e5");
+  test_real("8e4", "8e4");
+  test_real("8e3", "8000.0");
+  test_real("8e2", "800.0");
+  test_real("8e1", "80.0");
+  test_real("8e0", "8.0");
+  test_real("8e-1", "0.8");
+  test_real("8e-2", "0.08");
+  test_real("8e-3", "0.008");
+  test_real("8e-4", "8e-4");
+  test_real("8e-5", "8e-5");
+  test_real("8.13e6", "8.13e6");
+  test_real("8.13e5", "813000.0");
+  test_real("8.13e4", "81300.0");
+  test_real("8.13e3", "8130.0");
+  test_real("8.13e2", "813.0");
+  test_real("8.13e1", "81.3");
+  test_real("8.13e0", "8.13");
+  test_real("8.13e-1", "0.813");
+  test_real("8.13e-2", "0.0813");
+  test_real("8.13e-3", "0.00813");
+  test_real("8.13e-4", "8.13e-4");
+  test_real("8.13e-5", "8.13e-5");
+  test_real("8.1234567e6", "8.1234567e6");
+  test_real("8.1234567e5", "812345.67");
+  test_real("8.1234567e4", "81234.567");
+  test_real("8.1234567e3", "8123.4567");
+  test_real("8.1234567e2", "812.34567");
+  test_real("8.1234567e1", "81.234567");
+  test_real("8.1234567e0", "8.1234567");
+  test_real("8.1234567e-1", "0.81234567");
+  test_real("8.1234567e-2", "0.081234567");
+  test_real("8.1234567e-3", "0.0081234567");
+  test_real("8.1234567e-4", "8.1234567e-4");
+  test_real("8.1234567e-5", "8.1234567e-5");
+  test_real("-1.2e1", "-12.0");
+  test_real("-4.56e8", "-4.56e8");
+  test_real("1e-60", "1e-60");
+  test_real("1e80", "1e80");
+  test_real("NaN", "NaN");
+  test_real("Inf", "Inf");
+  test_real("-Inf", "-Inf");
+}
+#endif // #if defined(TEST_RYU_TO_HR)
+
+/*
+ * call this one from OMEdit or other clients to print doubles (without the trailing zero)!
+ * the caller needs to free the result.
+ */
+char* ryu_hr_tdzp(double r)
+{
+  char str1[32] = {0};
+  char buf[32] = {0};
+  d2s_buffered(r, str1);
+  // we don't need to have the trailing zero
+  ryu_to_hr(str1, buf, 0);
+  return strdup(buf);
+}
+
+/*
+ * call this one from OMEdit or other clients to print doubles (without the trailing zero)!
+ * the caller needs to provide the buffer (at least 32 chars)
+ */
+void ryu_hr_tdzp_buf(double r, char* buf)
+{
+  char str1[32] = {0};
+  d2s_buffered(r, str1);
+  // we don't need to have the trailing zero
+  ryu_to_hr(str1, buf, 0);
+}
+
+
